386e87da1b91469a575f56ff53e1d4b37400675c
davidgeorgiev <david_georgiev@mail.bg>
2016-11-26 21:41:47 +0200

    adding some functionality

--- ShowMap.php
+++ ShowMap.php
@@ -5,4 +5,35 @@ Description: Don't use this plugin because this is my first experiment and I don
 Author: David Georgiev
 Version: 0.0.0.1
 */
+
+add_action('init','show_google_map');
+
+function show_google_map(){
+	$myapikey = '';
+	echo '
+	<style>
+      #map {
+        height: 400px;
+        width: 100%;
+       }
+    </style>
+    <h3>My Google Maps Demo</h3>
+    <div id="map"></div>
+    <script>
+      function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key='.$myapikey.'&callback=initMap">
+    </script>';
+}
 ?>
