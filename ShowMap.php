6906e0843c80ebf28877d37f9dbcb6b6670d393a
David <david@david-Satellite-C660D.(none)>
2016-11-26 12:59:28 +0200

    uplading the main php file to git

---
+++ ShowMap.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: ShowMap
+Description: Don't use this plugin because this is my first experiment and I don't know what I'm doing because nobody told me how to make a wordpress plugin. Use it at your own risk. Have a nice day, dear wordpress user!
+Author: David Georgiev
+Version: 0.0.0.1
+*/
+?>
