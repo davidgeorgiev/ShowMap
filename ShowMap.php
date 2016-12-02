a792037ed5357dc23e15a4f42481acc12699ec1a
davidgeorgiev <david_georgiev@mail.bg>
2016-12-02 23:10:49 +0200

    adding function to reset tables and abbility to continue from last position

--- ShowMap.php
+++ ShowMap.php
@@ -8,12 +8,27 @@ Version: 1.0
 
 //add_action('init','show_my_google_map');
 
+function reset_tables(){
+	global $wpdb;
+	$myrows = $wpdb->get_results("DROP TABLE map_points;");
+	$myrows = $wpdb->get_results("DROP TABLE map_points_last;");
+	$myrows = $wpdb->get_results("CREATE TABLE map_points(map_point_id int,lat float,lng float);");
+	$myrows = $wpdb->get_results("CREATE TABLE map_points_last(last_max_counter int,last_i int, last_j int);");
+	$myrows = $wpdb->get_results("INSERT INTO map_points_last (last_max_counter ,last_i, last_j) VALUES(1,1,1);");
+}
+
 function search_cities_and_show($maxnum){
 	global $wpdb;
 	$counter = 0;
-	$myrows = $wpdb->get_results("TRUNCATE TABLE map_points;");
-	for($i = 1;$i <= 100; $i++){
-		for($j = 1; $j <= 100; $j++){	
+	$myrows = $wpdb->get_results("SELECT last_max_counter, last_i, last_j FROM map_points_last;");
+	foreach($myrows as $key => $row) {
+		$counter = $row->last_max_counter;
+		$i = $row->last_i;
+		$j = $row->last_j;
+	}
+	//$myrows = $wpdb->get_results("TRUNCATE TABLE map_points;");
+	while($i <= $maxnum){
+		while($j <= $maxnum){
 			$url = 'http://freegeoip.net/xml/'.$i.'.'.$j.'.1.1';
 			$xml = new SimpleXMLElement(file_get_contents($url));
 			if(($xml->Latitude!=0)&&($xml->Longitude!=0)){
@@ -21,9 +36,12 @@ function search_cities_and_show($maxnum){
 				//echo '<div><h1>Point: '.$counter.'</h1>';
 				//echo '<p>'.$xml->Latitude.'</p>';
 				//echo '<p>'.$xml->Longitude.'</p></div>';
+				$myrows = $wpdb->get_results("INSERT INTO map_points_last (last_max_counter ,last_i, last_j) VALUES(".$counter.",".$i.",".$j.");");
 				$myrows = $wpdb->get_results("INSERT INTO map_points (map_point_id ,lat, lng) VALUES(".$counter.",".$xml->Latitude.",".$xml->Longitude.")");
 			}
+			$j++;
 		}
+		$i++;
 	}
 }
 
@@ -31,7 +49,7 @@ function show_my_google_map(){
 	global $wpdb;
 	$myrows = $wpdb->get_results("SELECT map_point_id ,lat, lng FROM map_points;");
 
-	$myapikey = 'your google api key here';
+	$myapikey = 'your google api key';
 	echo '
 	<style>
       #map {
