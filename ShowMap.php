396940134132bf6792f983d404498957308db82e
davidgeorgiev <david_georgiev@mail.bg>
2016-12-08 12:48:54 +0200

    New server api communication because of banned freegeoip.net in the office, and added user filter from the url of the page where the plugin is running, checking for errors end more

--- ShowMap.php
+++ ShowMap.php
@@ -8,6 +8,48 @@ Version: 1.0
 
 //add_action('init','show_my_google_map');
 
+
+function get_lat_lng_from_nekudo_please($firstdigit,$seconddigit){
+	$server_error = 0;
+	$index = 0;
+	$url = 'http://geoip.nekudo.com/api/'.$firstdigit.'.'.$seconddigit.'.1.1';
+	$jsondata = file_get_contents($url);
+	$myarray = (json_decode($jsondata, true));
+	
+	if (is_array($myarray) || is_object($myarray)){
+		foreach($myarray as $key => $row1) {
+			if (is_array($row1) || is_object($row1)){
+				foreach($row1 as $key => $row2) {
+					$index++;
+					if($index == 4){
+						$latitude = $row2;
+					}
+					if($index == 5){
+						$longitude = $row2;
+					}
+					//echo '<p>'.$index.' '.$row2.'</p>';
+				}
+			}
+		}
+	}
+	if($latitude&&$longitude){
+		return array($latitude,$longitude);
+	}else{
+		return -1;
+	}
+}
+function get_lat_and_lng_from_freegeoip($firstdigit,$seconddigit){
+	$server_error = 0;
+	$url = 'http://freegeoip.net/xml/'.$firstdigit.'.'.$seconddigit.'.1.1';
+	$myFileContents = file_get_contents($url);
+	$xml = new SimpleXMLElement($myFileContents);
+	if(($xml->Latitude!=0)&&($xml->Longitude!=0)){
+		return array($xml->Latitude,$xml->Longitude);
+	}else{
+		return -1;
+	}
+}
+
 function reset_tables(){
 	global $wpdb;
 	$myrows = $wpdb->get_results("DROP TABLE map_points;");
@@ -29,13 +71,14 @@ function search_cities_and_show($maxnum){
 	//$myrows = $wpdb->get_results("TRUNCATE TABLE map_points;");
 	while($i <= $maxnum){
 		while($j <= $maxnum){
-			$url = 'http://freegeoip.net/xml/'.$i.'.'.$j.'.1.1';
-			$xml = new SimpleXMLElement(file_get_contents($url));
-			if(($xml->Latitude!=0)&&($xml->Longitude!=0)){
+			$lat_and_lng = get_lat_lng_from_nekudo_please($i,$j);
+			if($lat_and_lng!=-1){
+				$lat = $lat_and_lng[0];
+				$lng = $lat_and_lng[1];
 				//echo '<div><h1>Point: '.$counter.'</h1>';
-				//echo '<p>'.$xml->Latitude.'</p>';
-				//echo '<p>'.$xml->Longitude.'</p></div>';
-				$myrows = $wpdb->get_results("INSERT INTO map_points (map_point_id ,lat, lng) VALUES(".$counter.",".$xml->Latitude.",".$xml->Longitude.")");
+				//echo '<p>'.$lat.'</p>';
+				//echo '<p>'.$lng.'</p></div>';
+				$myrows = $wpdb->get_results("INSERT INTO map_points (map_point_id ,lat, lng) VALUES(".$counter.",".$lat.",".$lng.")");
 				$myrows = $wpdb->get_results("INSERT INTO map_points_last (last_max_counter ,last_i, last_j) VALUES(".$counter.",".$i.",".$j.");");
 				$counter++;
 			}
@@ -46,11 +89,25 @@ function search_cities_and_show($maxnum){
 	}
 }
 
-function show_my_google_map(){
-	global $wpdb;
-	$myrows = $wpdb->get_results("SELECT map_point_id ,lat, lng FROM map_points;");
+function ShowErronNoPointsInThisInterval(){
+	echo '<h1>No points found in this interval</h1>';
+}
 
-	$myapikey = 'your google api here';
+function show_my_google_map($minid,$maxid){
+	if($minid==0){
+		$minid=1;
+	}
+	if($maxid==0){
+		$maxid=10000;
+	}
+	global $wpdb;
+	$mysqlquery = "SELECT map_point_id ,lat, lng FROM map_points WHERE map_point_id <= ".$maxid." AND map_point_id >= ".$minid.";";
+	$myrows = $wpdb->get_results($mysqlquery);
+
+	$myapikey = 'your api key here';
+	if(empty($myrows)){
+		ShowErronNoPointsInThisInterval();
+	}
 	echo '
 	<style>
       #map {
@@ -66,8 +123,8 @@ function show_my_google_map(){
 		}
 	echo'
         var map = new google.maps.Map(document.getElementById("map"), {
-          zoom: 7,
-          center: uluru1
+          zoom: 1,
+          center: uluru'.$minid.'
         });';
         foreach($myrows as $key => $row) {
 			echo 'var marker = new google.maps.Marker({position: uluru'.$row->map_point_id.',map: map});';
@@ -78,5 +135,26 @@ function show_my_google_map(){
     <script async defer
     src="https://maps.googleapis.com/maps/api/js?key='.$myapikey.'&callback=initMap">
     </script>';
+}
+function mainGoogleMapDavidsPlugin($reset,$search,$show){
+	if($reset){
+		reset_tables();
+	}
+	if($search){
+		search_cities_and_show(255);
+	}
+	if($show){
+		if(isset($_GET['minid'])){
+			$myminid = $_GET['minid'];
+		}else{
+			$myminid = 0;
+		}
+		if(isset($_GET['maxid'])){
+			$mymaxid = $_GET['maxid'];
+		}else{
+			$mymaxid = 0;
+		}
+		show_my_google_map($myminid,$mymaxid);
+	}
 }
 ?>
