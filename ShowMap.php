3196bad3b4c32c2d4550b407314f398d6246fcd0
davidgeorgiev <david_georgiev@mail.bg>
2016-12-08 12:52:50 +0200

    changin the version number of the plugin

--- ShowMap.php
+++ ShowMap.php
@@ -3,7 +3,7 @@
 Plugin Name: ShowMap
 Description: Don't use this plugin because this is my first experiment and I don't know what I'm doing because nobody told me how to make a wordpress plugin. Use it at your own risk. Have a nice day, dear wordpress user!
 Author: David Georgiev
-Version: 1.0
+Version: 2.0
 */
 
 //add_action('init','show_my_google_map');
